b80324c9f3f9964543a2ed3c1afcb40c88afb7a8
oilujc <jcmartinezvalero@gmail.com>
2019-11-10 13:25:34 -0400

    added api features

---
+++ theme/assets/js/custom.js
@@ -0,0 +1,97 @@
+/*************************************
+* Theme Name: Jobvivo
+* Author: Themez Hub
+* Version: 1.0
+* Last Change: Dec 27 2017
+  Author URI    : http://www.themezhub.com/
+**************************************
+* 01. Testimonial 1 Script
+* 02. Employer Slide
+* 03. Category Slide
+* 04. Fast Click Select
+* 05. Bootstrap wysihtml5 editor
+* 06. Add field Script
+**************************************/
+(function($){
+	"use strict";
+
+	/*------ Testimonial 1 Script ----*/
+	$('.testimonial-carousel').slick({
+	  slidesToShow:2,
+	  arrows: false,
+	  autoplay:true,
+	  responsive: [
+		{
+		  breakpoint: 768,
+		  settings: {
+			arrows: false,
+			centerPadding: '0px',
+			slidesToShow:2
+		  }
+		},
+		{
+		  breakpoint: 480,
+		  settings: {
+			arrows: false,
+			centerPadding: '0px',
+			slidesToShow: 1
+		  }
+		}
+	  ]
+	});
+	
+	/*--- Employer Slide ---*/
+	$('.employer-slide').slick({
+	  centerMode: true,
+	  centerPadding: '0px',
+	  slidesToShow: 4,
+	  responsive: [
+		{
+		  breakpoint: 768,
+		  settings: {
+			arrows: false,
+			centerMode: true,
+			centerPadding: '0px',
+			slidesToShow: 3
+		  }
+		},
+		{
+		  breakpoint: 480,
+		  settings: {
+			arrows: false,
+			centerMode: true,
+			centerPadding: '0px',
+			slidesToShow: 1
+		  }
+		}
+	  ]
+	});
+	
+	/*---- Category Slide ---*/
+	$('.category-slide').slick({
+	  centerMode: true,
+	  centerPadding: '60px',
+	  slidesToShow: 3,
+	  responsive: [
+		{
+		  breakpoint: 768,
+		  settings: {
+			arrows: false,
+			centerMode: true,
+			centerPadding: '40px',
+			slidesToShow: 2
+		  }
+		},
+		{
+		  breakpoint: 480,
+		  settings: {
+			arrows: false,
+			centerMode: true,
+			centerPadding: '40px',
+			slidesToShow: 1
+		  }
+		}
+	  ]
+	});
+	
+})(jQuery);
